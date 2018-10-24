54bbc0022c2b4f2708b35d796d02cbc7e6401391
Biimka <olya_samolova@mail.ru>
2018-10-24 19:19:10 +0400

    TDL-4. Routing

--- app/src/main/java/com/startandroid/biimka/todolist/TaskActivity.java
+++ app/src/main/java/com/startandroid/biimka/todolist/TaskActivity.java
@@ -8,6 +8,5 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
-
     }
 }
