03fabbbc1e0903677a9c2eceb6cd7769c31ec13f
Biimka <olya_samolova@mail.ru>
2018-11-10 15:50:53 +0400

    TDL-7. SQLite

--- app/src/main/java/com/startandroid/biimka/todolist/TasksRepoImpl.java
+++ app/src/main/java/com/startandroid/biimka/todolist/TasksRepoImpl.java
@@ -1,5 +1,9 @@
 package com.startandroid.biimka.todolist;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +11,11 @@ public class TasksRepoImpl implements TasksRepo {
     private List<Task> tasks = new ArrayList<>();
     private long maxID = 1;
     private static final TasksRepoImpl ourInstance = new TasksRepoImpl();
+    private DBHelper dbHelper = new DBHelper(App.context);
+    private ContentValues cv = new ContentValues();
+    private SQLiteDatabase db = dbHelper.getWritableDatabase();
+    private Cursor cursor = db.query("mytable", null, null,
+            null, null, null, null);
 
     public static TasksRepoImpl getInstance() {
         return ourInstance;
@@ -17,6 +26,17 @@ public class TasksRepoImpl implements TasksRepo {
 
     @Override
     public List<Task> getTasks() {
+        if (cursor.moveToFirst()) {
+            int idColIndex = cursor.getColumnIndex("id");
+            int nameColIndex = cursor.getColumnIndex("taskName");
+            int isCompletedColIndex = cursor.getColumnIndex("isCompleted");
+            boolean isCompletedBool = isCompletedColIndex == 1;
+            do {
+                tasks.add(new Task(cursor.getInt(idColIndex),
+                        cursor.getString(nameColIndex), isCompletedBool));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
         return tasks;
     }
 
@@ -33,6 +53,9 @@ public class TasksRepoImpl implements TasksRepo {
     public void createTask(String name) {
         tasks.add(new Task(maxID, name, false));
         maxID++;
+        cv.put("taskName", name);
+        cv.put("isCompleted", false);
+        db.insert("mytable", null, cv);
     }
 
     @Override
@@ -42,6 +65,14 @@ public class TasksRepoImpl implements TasksRepo {
             return;
         }
         tasks.set(position, task);
+        int isCompleted;
+        if (!task.isCompleted()) isCompleted = 0;
+        else isCompleted = 1;
+        cv.put("id", task.getId());
+        cv.put("taskName", task.getName());
+        cv.put("isCompleted", isCompleted);
+        db.update("mytable", cv, "id = ?",
+                new String[]{String.valueOf(task.getId())});
     }
 
     @Override
