f524524e4d73077bbb82cffa0cc81e13b04e3070
Biimka <olya_samolova@mail.ru>
2018-11-14 20:47:10 +0400

    TDL-7. getTask(), getTasks(), deleteTask()

--- app/src/main/java/com/startandroid/biimka/todolist/TasksRepoImpl.java
+++ app/src/main/java/com/startandroid/biimka/todolist/TasksRepoImpl.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TasksRepoImpl implements TasksRepo {
-    private long maxID = 1;
     private static final TasksRepoImpl ourInstance = new TasksRepoImpl();
     private DBHelper dbHelper = new DBHelper(App.context);
     private SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -28,11 +27,11 @@ public class TasksRepoImpl implements TasksRepo {
         final Cursor tasksCursor = db.query("Tasks", null, null,
                 null, null, null, null);
         if (tasksCursor.moveToFirst()) {
-            int idColIndex = tasksCursor.getColumnIndex("id");
-            int nameColIndex = tasksCursor.getColumnIndex("taskName");
-            int isCompletedColIndex = tasksCursor.getColumnIndex("isCompleted");
-            boolean isCompletedBool = tasksCursor.getInt(isCompletedColIndex) == 1;
+            final int idColIndex = tasksCursor.getColumnIndex("id");
+            final int nameColIndex = tasksCursor.getColumnIndex("taskName");
+            final int isCompletedColIndex = tasksCursor.getColumnIndex("isCompleted");
             do {
+                final boolean isCompletedBool = tasksCursor.getInt(isCompletedColIndex) == 1;
                 tasks.add(new Task(tasksCursor.getInt(idColIndex),
                         tasksCursor.getString(nameColIndex), isCompletedBool));
             } while (tasksCursor.moveToNext());
@@ -43,18 +42,22 @@ public class TasksRepoImpl implements TasksRepo {
 
     @Override
     public Task getTask(long id) {
-        final int position = getIndex(id);
-        if (position < 0) {
-            return null;
+        final Cursor tasksCursor = db.query("Tasks", null, "id = ?",
+                new String[]{String.valueOf(id)}, null, null, null);
+        if (tasksCursor.moveToFirst()) {
+            final boolean isCompletedBool = tasksCursor
+                    .getInt(tasksCursor.getColumnIndex("isCompleted")) == 1;
+            return new Task(tasksCursor.getInt(tasksCursor.getColumnIndex("id")),
+                    tasksCursor.getString(tasksCursor.getColumnIndex("taskName")),
+                    isCompletedBool);
         }
-        return getTasks().get(position);
+        tasksCursor.close();
+        return null;
     }
 
     @Override
     public void createTask(String name) {
         final ContentValues contentValues = new ContentValues();
-        getTasks().add(new Task(maxID, name, false));
-        maxID++;
         contentValues.put("taskName", name);
         contentValues.put("isCompleted", 0);
         db.insert("Tasks", null, contentValues);
@@ -63,10 +66,7 @@ public class TasksRepoImpl implements TasksRepo {
     @Override
     public void updateTask(Task task) {
         final ContentValues contentValues = new ContentValues();
-        final int position = getIndex(task.getId());
-        if (position < 0) return;
-        getTasks().set(position, task);
-        int isCompleted;
+        final int isCompleted;
         if (!task.isCompleted()) isCompleted = 0;
         else isCompleted = 1;
         contentValues.put("id", task.getId());
@@ -78,21 +78,7 @@ public class TasksRepoImpl implements TasksRepo {
 
     @Override
     public void deleteTask(long id) {
-        final int position = getIndex(id);
-        if (position < 0) {
-            return;
-        }
-        getTasks().remove(position);
-    }
-
-    private int getIndex(long id) {
-        final List<Task> tasks = getTasks();
-        for (int i = 0; i < tasks.size(); i++) {
-            if (id == tasks.get(i).getId()) {
-                return i;
-            }
-        }
-        return -1;
+        db.delete("Tasks", "id = " + id, null);
     }
 }
 
